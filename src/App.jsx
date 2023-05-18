fe544951ffca2d4a5967e776f839071585f77374
tayyaba-ali <ta6402792@gmail.com>
2023-05-18 15:09:15 +0500

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+		<>
+			<h1>Hello World</h1>
+			<img src={reactLogo} alt='' />
+			<img src={viteLogo} alt='' />
+		</>
+	);
+}
+
+export default App
